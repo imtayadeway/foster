0598cd08874899c7f320ec95ac0a7dbffa9f0aba
Tim Wade <hello@timjwade.com>
2016-09-11 11:26:07 -0700

    mv publishers

--- lib/foster/publishers.rb
+++ lib/foster/publishers.rb
@@ -1,7 +1,7 @@
 require "yaml"
 
 Foster::Base.class_eval do
-  PUBLISHERS = YAML.load_file(File.join(File.dirname(__FILE__), "..", "publishers.yaml"))
+  PUBLISHERS = YAML.load_file(File.join(File.dirname(__FILE__), "..", "..", "data", "publishers.yaml"))
 
   def publisher
     @publisher ||= PUBLISHERS[registration_group][registrant]
